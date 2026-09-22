027e826934c937dc07b1bb239560385227c07112
RudeAnt <huntergeorgod@gmail.com>
2026-09-23 00:24:30 +0300

    reworked docker for new frontend, edited old backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { TicketsModule } from './tickets/tickets.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT || '5433', 10),
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'rootpassword',
+      database: process.env.DATABASE_NAME || 'domax',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TicketsModule,
+    // TicketsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
